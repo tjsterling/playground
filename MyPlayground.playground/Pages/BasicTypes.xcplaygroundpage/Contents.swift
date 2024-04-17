2531b619cbcf5a23bab2a86a1f165a0c6e2e581b
TJ Sterling <t0001@outlook.de>
2024-04-18 00:19:02 +0200

    basic types

--- MyPlayground.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -1,2 +1,32 @@
 
 import Foundation
+
+// String is "regular Text"
+let myFirstItem = "Hello Wolrd"
+
+let mySecondItem: String = "Hello World"
+
+let myThirdItem = myFirstItem + " test"
+
+
+// Boolean (bool)
+let myFirstBool = true
+
+let mySecondBool: Bool = false
+
+//Swift is a type save language
+// trying to change type will result in a error
+//let typeSafeBool: Bool = "true"
+
+//Date
+let myFirstDate: Date = Date()
+
+//Number can be Int, Double CGFloat & more
+
+let numberOne: Int = 1
+
+//Best used for Math
+let numberTwo: Double = 1.0
+
+//Best used for UI
+let numberThree: CGFloat = 1.0

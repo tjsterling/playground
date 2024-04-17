a9706b89579d795462d09f42936578ad4fcf4c9c
TJ Sterling <t0001@outlook.de>
2024-04-17 23:59:43 +0200

    basics

--- MyPlayground.playground/Pages/Swiftbasics.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Swiftbasics.xcplaygroundpage/Contents.swift
@@ -2,3 +2,27 @@
 import Foundation
 
 var greeting = "Hello, playground"
+
+print(greeting)
+
+// single line comment
+
+/*
+ multi line comment
+ */
+
+//naming conventions
+
+// camel case
+// swift best practice
+
+let firstGreeting = "Hello Wolrd"
+let thisIsMyFirstGreeting = ""
+
+// snake case
+
+let this_is_snake_case = ""
+
+// pascal case
+
+let ThisIsPacalCase = ""
